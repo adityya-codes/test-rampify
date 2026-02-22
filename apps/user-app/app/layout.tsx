d7ea4c78441e17a669aa199d5620a0a5d5af27fd
Aditya-Ramola <workwithaditya001@gmail.com>
2026-02-22 21:05:09 +0530

    rampify basic setup

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/providers";
+
 
 
 const geistSans = Geist({
@@ -27,9 +29,9 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        
+      <Providers>
           {children}
-        
+          </Providers>
       </body>
 
     </html>
